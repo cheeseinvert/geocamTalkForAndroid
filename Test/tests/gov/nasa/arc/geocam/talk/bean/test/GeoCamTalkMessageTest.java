2bae0a75a525e6af9c3ae337fb3e77420d55f4a7
Sam Karp <sam.karp@sv.cmu.edu>
2011-03-30 20:48:18 -0600

    Added year to Talk tests date formatter

--- Test/tests/gov/nasa/arc/geocam/talk/bean/test/GeoCamTalkMessageTest.java
+++ Test/tests/gov/nasa/arc/geocam/talk/bean/test/GeoCamTalkMessageTest.java
@@ -15,14 +15,14 @@ public class GeoCamTalkMessageTest extends TestCase {
 	{
 		
 		// arrange
-		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd hh:mm:ss");
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy hh:mm:ss");
 				
 		GeoCamTalkMessage message1 = new GeoCamTalkMessage();
 		message1.setAuthorUsername("rhornsby");
 		message1.setAuthorFullname("Rufus Hornsby");
 		message1.setLongitude(-122.057954);
 		message1.setContent("Structural engineer not allowing access to building. Fire is too out of control. Fire squad alerted.");
-		message1.setContentTimestamp(sdf.parse("03/13 10:48:44"));
+		message1.setContentTimestamp(sdf.parse("03/13/11 10:48:44"));
 		message1.setLatitude(37.411629);
 		message1.setMessageId(15);
 		message1.setAccuracy(60);
@@ -33,7 +33,7 @@ public class GeoCamTalkMessageTest extends TestCase {
 		message2.setAuthorFullname("Rufus Hornsby");
 		message2.setLongitude(-122.057954);
 		message2.setContent("Structural engineer not allowing access to building. Fire is too out of control. Fire squad alerted.");
-		message2.setContentTimestamp(sdf.parse("03/13 10:48:44"));
+		message2.setContentTimestamp(sdf.parse("03/13/11 10:48:44"));
 		message2.setLatitude(37.411629);
 		message2.setMessageId(15);
 		message2.setAccuracy(60);
@@ -46,14 +46,14 @@ public class GeoCamTalkMessageTest extends TestCase {
 	public void testEqualsMethodFalse() throws Exception
 	{ 
 		// arrange
-		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd hh:mm:ss");
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy hh:mm:ss");
 				
 		GeoCamTalkMessage message1 = new GeoCamTalkMessage();
 		message1.setAuthorUsername("rhornsby");
 		message1.setAuthorFullname("Rufus Hornsby");
 		message1.setLongitude(-122.057954);
 		message1.setContent("Structural engineer not allowing access to building. Fire is too out of control. Fire squad alerted.");
-		message1.setContentTimestamp(sdf.parse("03/13 10:48:44"));
+		message1.setContentTimestamp(sdf.parse("03/13/11 10:48:44"));
 		message1.setLatitude(37.411629);
 		message1.setMessageId(15);
 		message1.setAccuracy(60);
@@ -64,7 +64,7 @@ public class GeoCamTalkMessageTest extends TestCase {
 		message2.setAuthorFullname("Rufus Hornsby");
 		message2.setLongitude(-122.057954);
 		message2.setContent("Structural engineer not allowing access to building. Fire is too out of control. Fire squad alerted. Blah"); // changed here
-		message2.setContentTimestamp(sdf.parse("03/13 10:48:44"));
+		message2.setContentTimestamp(sdf.parse("03/13/11 10:48:44"));
 		message2.setLatitude(37.411629);
 		message2.setMessageId(15);
 		message2.setAccuracy(60);
