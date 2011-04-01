2bcd321f5c379463322c6d461303cd50f39cfa0e
patrickbaumann <baumannpat@gmail.com>
2011-03-31 17:49:16 -0700

    Fixed incorrectly formated test json feed and swapped regular expression parsing for gson JsonParser use

--- android/src/gov/nasa/arc/geocam/talk/service/DjangoTalkJsonConverterImplementation.java
+++ android/src/gov/nasa/arc/geocam/talk/service/DjangoTalkJsonConverterImplementation.java
@@ -11,6 +11,8 @@ import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 
 public class DjangoTalkJsonConverterImplementation
@@ -18,21 +20,18 @@ public class DjangoTalkJsonConverterImplementation
 
 	@Override
 	public List<GeoCamTalkMessage> deserializeList(String jsonString) {
-		Pattern jsonList = Pattern.compile("^.*\"ms\":\\s+(.*)\\}$");
-		Matcher m = jsonList.matcher(jsonString);
-
-		if  (m.find()){
-			GsonBuilder builder = new GsonBuilder();
-			builder.setDateFormat("MM/dd/yy HH:mm:ss");
-			Gson gson = builder.create();
-			
-			Type listType = new TypeToken<List<GeoCamTalkMessage>>(){}.getType();
-			
-			return gson.fromJson(m.group(1), listType);
-		} else {
-			return null;
-		}
+		JsonParser parser = new JsonParser();
+		JsonObject root = parser.parse(jsonString).getAsJsonObject();
 		
+		String jsonMessages = root.get("ms").toString();
+		
+		GsonBuilder builder = new GsonBuilder();
+		builder.setDateFormat("MM/dd/yy HH:mm:ss");
+		Gson gson = builder.create();
+		
+		Type listType = new TypeToken<List<GeoCamTalkMessage>>(){}.getType();
+		
+		return gson.fromJson(jsonMessages, listType);
 	}
 
 	public GeoCamTalkMessage deserialize(String jsonString) {
